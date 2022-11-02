c4f44e5664b27464e78fb26bc8ef7a0faada913c
Lingwuxin <1612469302@qq.com>
2022-11-02 14:33:56 +0800

    Update connectDB.py

--- app/data/connectDB.py
+++ app/data/connectDB.py
@@ -1,16 +1,29 @@
 import pymysql
 from pymysql import cursors
+
+
 def contect_database(func):
     def wrapper():
-        connect=pymysql.connect(host='127.0.0.1',user='yolo_garbage_window',password='123456')
-        cursor=connect.cursor()
-        res=func(cursor)
+        connect = pymysql.connect(
+            host="127.0.0.1", user="yolo_garbage_window", password="123456"
+        )
+        cursor = connect.cursor()
+        res = func(cursor)
         connect.close()
         return res
+
     return wrapper
 
-@contect_database        
-def test(cursor:cursors.Cursor):
-    a=cursor.execute('show databases')
-    print(cursor.fetchmany(a))
-test()
+
+@contect_database
+def save_site(cursor: cursors.Cursor):
+    pass
+
+
+@contect_database
+def select_site(cursor: cursors.Cursor):
+    pass
+
+@contect_database
+def select_object(cursor:cursors.Cursor):
+    pass
